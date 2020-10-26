42b932186ec5c04d39fb32f52b551b6922cacd48
mcswift <develop@mcswift.com>
2020-10-26 09:47:07 +0800

    基本验证

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func test() {
-	robotgo.ScrollMouse(100, "up")
+	robotgo.MoveMouse(50, 50)
 }
 
 func main() {
@@ -15,6 +15,9 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/test", func(c *gin.Context) {
+		test()
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
